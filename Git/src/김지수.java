5ff780505a6a0d2f885f5a3cda498d96c6c47817
배준규 <icom009@gmail.com>
2026-01-06 12:31:26 +0900

    Delete Git directory

--- Git/src/김지수.java
+++
@@ -1,9 +0,0 @@
-
-public class 김지수 {
-
-	public static void main(String[] args) {
-		System.out.println("김지수");
-
-	}
-
-}
--- Git/src/박지호.java
+++
@@ -1,9 +0,0 @@
-
-public class 박지호 {
-
-	public static void main(String[] args) {
-		// 건들지마소
-
-	}
-
-}
