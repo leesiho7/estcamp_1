0fe2ed0b868eaf0e5f115e9ca77d4caae1d3d89b
zzushu <kissday0415@gmail.com>
2026-01-06 15:50:04 +0900

    김지수5-유저랭킹수정1

--- musicquiz/src/model/user_ranking.java
+++ musicquiz/src/model/user_ranking.java
@@ -4,6 +4,7 @@ public class user_ranking {
 	private String userId;
 	private int correctNumber;
 	private int raking;
+	private int point;
 	
 
 	// Getter // Setter
@@ -25,18 +26,25 @@ public class user_ranking {
 	public void setRanking(int raking) {
 		this.raking = raking;
 	}
+	public int getPoint() {
+		return point;
+	}
+	public void setpoint(int point) {
+		this.point = point;
+	}
 
 	// toString
 	@Override
 	public String toString() {
-		return "User_Ranking [userId=" + userId+ ", number=" + correctNumber + ", raking=" + raking + "]";
+		return "User_Ranking [userId=" + userId+ ", number=" + correctNumber + ", raking=" + raking+ ", point=" + point + "]";
 	}
 
 	// 기본생성자 // 전체 멤버를 필요로하는 생성자
-	public user_ranking (String userId, int correctNumber, int raking) {
+	public user_ranking (String userId, int correctNumber, int raking, int point) {
 		this.userId = userId;
 		this.correctNumber = correctNumber;
 		this.raking = raking;
+		this.point = point;
 	}
 
 	public user_ranking () {
