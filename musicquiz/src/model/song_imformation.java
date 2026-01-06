8fdd51d162a413e42e148d7cd1cfae04742b9d12
zzushu <kissday0415@gmail.com>
2026-01-06 15:12:19 +0900

    김지수2-곡 정보

---
+++ musicquiz/src/model/song_imformation.java
@@ -0,0 +1,35 @@
+package model;
+
+public class song_imformation {
+private String sigerName;
+private String songName;
+
+// Getter // Setter
+public String getsigerName() {
+	return sigerName;
+}
+public void setsigerName(String sigerName) {
+	this.sigerName = sigerName;
+}
+public String getsongName() {
+	return songName;
+}
+public void setsongName(String songName) {
+	this.songName = songName;
+}
+
+
+
+// toString
+@Override
+public String toString() {
+	return "Song_Imformation [siger name=" + sigerName + ", song name=" + songName + "]";
+}
+
+// 기본생성자 // 전체 멤버를 필요로하는 생성자
+public song_imformation (String userId, String userPw) {
+	this.sigerName = sigerName;
+	this.songName = songName;
+
+}
+}
