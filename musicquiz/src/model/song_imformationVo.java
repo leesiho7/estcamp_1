8856efb1bee91ac31cbd23f77c62256dfb6d07ac
zzushu <kissday0415@gmail.com>
2026-01-07 10:12:36 +0900

    김지수7-모델수정

--- musicquiz/src/model/song_imformationVo.java
+++ musicquiz/src/model/song_imformationVo.java
@@ -3,6 +3,8 @@ package model;
 public class song_imformationVo {
 private String sigerName;
 private String songName;
+private int correctedNumber;
+private int songRaking;
 
 // Getter // Setter
 public String getsigerName() {
@@ -17,19 +19,36 @@ public String getsongName() {
 public void setsongName(String songName) {
 	this.songName = songName;
 }
+public int getcorrectedNumber() {
+	return correctedNumber;
+}
+
+public void setcorrectedNumber(int correctedNumber) {
+	this.correctedNumber = correctedNumber;
+}
+
+public int getsongRanking() {
+	return songRaking;
+}
+
+public void setsongRanking(int songRaking) {
+	this.songRaking = songRaking;
+}
 
 
 
 // toString
 @Override
 public String toString() {
-	return "Song_Imformation [siger name=" + sigerName + ", song name=" + songName + "]";
+	return "Song_Imformation [siger name=" + sigerName + ", song name=" + songName+ ", corrected number=" + correctedNumber + ", song raking=" + songRaking + "]";
 }
 
 // 기본생성자 // 전체 멤버를 필요로하는 생성자
-public song_imformationVo (String userId, String userPw) {
+public song_imformationVo (String userId, String userPw, int correctNumber, int raking) {
 	this.sigerName = sigerName;
 	this.songName = songName;
+	this.correctedNumber = correctedNumber;
+	this.songRaking = songRaking;
 
 }
 }
