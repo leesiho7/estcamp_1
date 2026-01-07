2b0b09cb93392b9c7ba9d3eaaedf7f0dfb057a6e
zzushu <kissday0415@gmail.com>
2026-01-07 12:07:37 +0900

    김지수10 - 컨트롤러 테스트

--- musicquiz/src/controller/Main.java
+++ musicquiz/src/controller/Main.java
@@ -1,11 +1,18 @@
 package controller;
 
+import java.util.ArrayList;
+
 import model.User_rankingDAO;
+import model.User_rankingVo;
 
 public class Main {
 
 	public static void main(String[] args) {
-		User_rankingDAO dao1 = User_rankingDAO();
+		User_rankingDAO dao1 = new User_rankingDAO();
+		ArrayList<User_rankingVo> result = dao1.selectAll();
+		System.out.println(result);
 	}
+
+	
 
 }
